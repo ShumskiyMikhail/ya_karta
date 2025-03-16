7efa0151de0253bfbf8bb389d9a42c61a493f659
bombordilo crocodilo <152640314+ShumskiyMikhail@users.noreply.github.com>
2025-03-16 12:27:36 +0300

    Add files via upload

--- PythonProject1/main.py
+++ PythonProject1/main.py
@@ -3,13 +3,12 @@ import requests
 from pprint import pprint
 from PIL import Image
 
-r = requests.get('https://olimp.miet.ru/ppo_it/api').json()
-i = 1
-
-for e in r['message']['data']:
-    print(*e, sep='\t')
-
 ans = []
+
+for i in range(4):
+    ans.append([])
+    for j in range(4):
+        ans[i].append(requests.get('https://olimp.miet.ru/ppo_it/api').json()['message']['data'])
 
 im = Image.new("RGB", (256, 256), (0, 0, 0))
 pixels = im.load()
@@ -17,11 +16,9 @@ pixels = im.load()
 for n1 in range(4):
     ans.append([])
     for n2 in range(4):
-        r = requests.get('https://olimp.miet.ru/ppo_it/api').json()['message']['data']
+        r = ans[n1][n2]
         for i in range(64):
             for j in range(64):
                 pixels[n2 * 64 + j, n1 * 64 + i] = r[i][j], r[i][j], r[i][j]
 
 im.save('res.jpg')
-
-
