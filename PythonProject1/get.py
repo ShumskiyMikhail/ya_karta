ce160e7ff764480b30d227f3a4124951cb590f5c
bombordilo crocodilo <152640314+ShumskiyMikhail@users.noreply.github.com>
2025-03-16 14:13:56 +0300

    Add files via upload

---
+++ PythonProject1/get.py
@@ -0,0 +1,5 @@
+from flask import Flask, request
+import requests
+from pprint import pprint
+from PIL import Image
+from pprint import pprint
--- PythonProject1/main.py
+++ PythonProject1/main.py
@@ -3,12 +3,109 @@ import requests
 from pprint import pprint
 from PIL import Image
 
+def corners(sp):
+    f1 = 0
+    f2 = 0
+    f3 = 0
+    f4 = 0
+    for i in range(4):
+        for j in range(4):
+            if sp[i][j][63][63] == 255 and sp[i][j][0][0] == 255 and sp[i][j][63][0] == 255 and f3 == 0:
+                sp[i][j], sp[3][0] = sp[3][0], sp[i][j]
+                f3 = 1
+
+            if sp[i][j][63][63] == 255 and sp[i][j][0][0] == 255 and sp[i][j][0][63] == 255 and f2 == 0:
+                sp[i][j], sp[0][3] = sp[0][3], sp[i][j]
+                f2 = 1
+
+            if sp[i][j][63][0] == 255 and sp[i][j][0][0] == 255 and sp[i][j][0][63] == 255 and f1 == 0:
+                sp[i][j], sp[0][0] = sp[0][0], sp[i][j]
+                f1 = 0
+
+            if sp[i][j][63][63] == 255 and sp[i][j][63][0] == 255 and sp[i][j][0][63] ==  255 and f4 == 0:
+                sp[i][j], sp[3][3] = sp[3][3], sp[i][j]
+                f4 = 0
+    return sp
+
+def left(sp):
+    l = 0
+    for i in range(4):
+        for j in range(4):
+            if (i, j) != (0, 0) and (i, j) != (3, 0) and (i, j) != (0, 3) and (i, j) != (3, 3):
+                if sp[i][j][32][0] == 255:
+                    if l == 0:
+                        sp[i][j], sp[1][0] = sp[1][0], sp[i][j]
+                        l = 1
+                    if l == 1:
+                        sp[i][j], sp[2][0] = sp[2][0], sp[i][j]
+                        return sp
+    return sp
+
+def right(sp):
+    r = 0
+    for i in range(4):
+        for j in range(1, 4):
+            if (i, j) != (0, 0) and (i, j) != (3, 0) and (i, j) != (0, 3) and (i, j) != (3, 3):
+                if sp[i][j][32][63] == 255:
+                    if r == 0:
+                        sp[i][j], sp[1][3] = sp[1][3], sp[i][j]
+                        r = 1
+                    else:
+                        sp[i][j], sp[2][3] = sp[2][3], sp[i][j]
+                        return sp
+    return sp
+
+def top(sp):
+    t = 0
+    for i in range(4):
+        for j in range(1, 3):
+            if (i, j) != (0, 0) and (i, j) != (3, 0) and (i, j) != (0, 3) and (i, j) != (3, 3):
+                if sp[i][j][0][32] == 255:
+                    if t == 0:
+                        sp[i][j], sp[0][1] = sp[0][1], sp[i][j]
+                        t = 1
+                    else:
+                        sp[i][j], sp[0][2] = sp[0][2], sp[i][j]
+                        return sp
+    return sp
+
+def bottom(sp):
+    b = 0
+    for i in range(1, 4):
+        for j in range(1, 3):
+            if (i, j) != (0, 0) and (i, j) != (3, 0) and (i, j) != (0, 3) and (i, j) != (3, 3):
+                if sp[i][j][63][32] == 255:
+                    if b == 0:
+                        sp[i][j], sp[3][1] = sp[3][1], sp[i][j]
+                        b = 1
+                    else:
+                        sp[i][j], sp[3][2] = sp[3][2], sp[i][j]
+                        return sp
+    return sp
+
+
+
 ans = []
 
+while len(ans) < 16:
+    s = (requests.get('https://olimp.miet.ru/ppo_it/api').json()['message']['data'])
+    if s not in ans:
+        ans.append(s)
+
+an = []
+
 for i in range(4):
-    ans.append([])
+    an.append([])
     for j in range(4):
-        ans[i].append(requests.get('https://olimp.miet.ru/ppo_it/api').json()['message']['data'])
+        an[i].append(ans[i * 4 + j])
+
+print(an)
+
+ans = corners(an)
+ans = left(ans)
+ans = right(ans)
+ans = top(ans)
+ans = bottom(ans)
 
 im = Image.new("RGB", (256, 256), (0, 0, 0))
 pixels = im.load()
